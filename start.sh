0c87c74f39fbe8297a2ee094b117f94b49516bfa
Masahito Kagawa <mkagawa@hotmail.com>
2017-05-12 09:34:27 -0700

    example log dir

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "starting repeaterConnector"
-./repeaterconnector -callsign KK6JA -mod1 A,20011 -mod2 B,20013 -rptcmd ../OpenDV/DStarRepeater/dstarrepeaterd -rcfg /usr/local/etc/opendv/dstarrepeater -logdir /var/log -confdir .
+./repeaterconnector -callsign KK6JA -mod1 A,20011 -mod2 B,20013 -rptcmd ../OpenDV/DStarRepeater/dstarrepeaterd -rcfg /usr/local/etc/opendv/dstarrepeater -logdir ./log -confdir .
 
